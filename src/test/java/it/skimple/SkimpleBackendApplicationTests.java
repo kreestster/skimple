bd3117eff16b2a353451148046602eb1d706a62f
LENOVO-PC-ANTO\Antonio <antoniomungari@libero.it>
2017-12-05 19:12:51 +0100

    first commit

---
+++ src/test/java/it/skimple/SkimpleBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package it.skimple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkimpleBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
